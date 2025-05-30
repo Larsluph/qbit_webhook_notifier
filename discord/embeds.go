9bf18e22242dc421ecb746e8c088aca2c20631b7
Rémi Marseault <larsluph@larsluph.dev>
2025-05-30 23:28:50 +0200

    fix: Escape webhook description to prevent formatting

--- discord/embeds.go
+++ discord/embeds.go
@@ -3,15 +3,21 @@ package discord
 import (
 	"qbit_webhook/helpers"
 	"qbit_webhook/qbit"
+	"regexp"
 	"time"
 )
+
+func escapeMarkdown(text string) string {
+	re := regexp.MustCompile(`([\\*_~` + "`" + `|[\]()])`)
+	return re.ReplaceAllString(text, `\\$1`)
+}
 
 func GenerateAddedEmbed(torrentProps qbit.TorrentProps) WebhookPayload {
 	return WebhookPayload{
 		Embeds: []Embed{
 			{
 				Title:       "New torrent added",
-				Description: torrentProps.Name,
+				Description: escapeMarkdown(torrentProps.Name),
 				Color:       0xFF0000,
 				Fields: []Field{
 					{
@@ -34,7 +40,7 @@ func GenerateCompletedEmbed(torrentProps qbit.TorrentProps) WebhookPayload {
 		Embeds: []Embed{
 			{
 				Title:       "Torrent completed",
-				Description: torrentProps.Name,
+				Description: escapeMarkdown(torrentProps.Name),
 				Color:       0x00FF00,
 				Fields: []Field{
 					{
@@ -67,7 +73,7 @@ func GenerateErrorEmbed(error helpers.ErrorPayload) WebhookPayload {
 		Content: "",
 		Embeds: []Embed{{
 			Title:       "Error Occured",
-			Description: error.Description,
+			Description: escapeMarkdown(error.Description),
 			Color:       0xCA0BE4,
 			Footer:      Footer{Text: error.CodeLocation},
 			Datetime:    time.Now().Format(time.RFC3339),
